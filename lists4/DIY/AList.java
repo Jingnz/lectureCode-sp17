17b34736cf233c95e5d9fb615b288ed1c12a3f0d
Jinjing Bao <jinjingbao.z@gmail.com>
2021-12-19 15:37:40 +0000

    completed Alist

--- lists4/DIY/AList.java
+++ lists4/DIY/AList.java
@@ -3,31 +3,45 @@
  */
 
 public class AList {
+    private int size;
+    private int[] items;
+
     /** Creates an empty list. */
     public AList() {
+        items = new int[100];
+        size = 0;
     }
-
+    /** Resizes the array to the target capacity.*/
+    private void resize(int capacity){
+        int[]a = new int[capacity];
+        System.arraycopy(items,0,a,0,size);
+        items = a;
+    }
     /** Inserts X into the back of the list. */
     public void addLast(int x) {
+        if(items.length == size){
+            resize(size+1);
+        }
+        items[size] = x;
+        size ++;
     }
-
     /** Returns the item from the back of the list. */
     public int getLast() {
-        return 0;        
+        return items[size-1];
     }
     /** Gets the ith item in the list (0 is the front). */
     public int get(int i) {
-        return 0;        
+        return items[i];
     }
-
     /** Returns the number of items in the list. */
     public int size() {
-        return 0;        
+        return size;
     }
-
     /** Deletes item from back of the list and
       * returns deleted item. */
     public int removeLast() {
-        return 0;
+        int x = getLast();
+        size --; //don't need to do anything to destroy the last item, it's there ,but not really there.
+        return x;
     }
 } 
